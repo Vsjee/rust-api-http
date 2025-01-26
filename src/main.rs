5dd81bc7f099cf5014b4bb7a2a13b7da9ab08c1d
David Hernandez <davidfelipehernandez1@gmail.com>
2025-01-26 17:51:34 -0500

    feat: implement MongoDB connection and movie retrieval in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #[macro_use]
 extern crate rocket;
 
+use mongodb::{
+    bson::{doc, Document},
+    Collection,
+};
+
 use dotenv::dotenv;
 
+mod conf;
 mod constants;
 mod models;
 mod routes;
 
 use routes::user_routes::*;
+
+use crate::conf::db_conf::DBConf;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -25,6 +33,27 @@ fn index() -> &'static str {
 async fn main() -> Result<(), rocket::Error> {
     // load .env file
     dotenv().ok();
+
+    // connect to MongoDB
+    let client = DBConf::connect().await;
+
+    let db: mongodb::Database = client.database("rust-api-http");
+    println!("Connected to MongoDB: {:?}", db);
+
+    println!("-----------");
+    let database = client.database("sample_mflix");
+    let my_coll: Collection<Document> = database.collection("movies");
+    let my_movie = match my_coll
+        .find_one(doc! { "title": "The Perils of Pauline" })
+        .await
+    {
+        Ok(movie) => movie,
+        Err(e) => {
+            eprintln!("Failed to find movie: {}", e);
+            std::process::exit(1);
+        }
+    };
+    println!("Found a movie:\n{:#?}", my_movie);
 
     // start rocket server and configure routes
     let _rocket = rocket::build()
