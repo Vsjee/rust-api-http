422bed04d1979c4e591b71c62d35a0fb0c8adbf3
David Hernandez <davidfelipehernandez1@gmail.com>
2025-01-26 16:10:26 -0500

    refactor constants: remove commented-out code for MONGO_URI

--- src/constants/constants.rs
+++ src/constants/constants.rs
@@ -1,12 +1,6 @@
 use lazy_static::lazy_static;
 use std::env;
 
-// lazy_static! {
-//     pub static ref MONGO_URI: String = {
-//         env::var("MONGO_DB_STRING_CONNECTION").expect("KEY must be set")
-//     }
-// }
-
 lazy_static! {
     pub static ref MONGO_URI: String =
         env::var("MONGO_DB_STRING_CONNECTION").expect("MONGO_DB_STRING_CONNECTION must be set");
