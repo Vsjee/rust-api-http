aef4a7d57465b8333ca05cff2c63fcf84c89e0f8
David Hernandez <davidfelipehernandez1@gmail.com>
2025-01-26 23:04:19 -0500

    feat: update get_user route to retrieve movie details and handle errors

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -1,11 +1,27 @@
-use crate::models::user_model::User;
-use rocket::serde::json::Json;
+use crate::{conf::db_conf::DBCollections, models::user_model::User};
+use mongodb::bson::doc;
+use rocket::{http::Status, serde::json::Json, State};
 
 #[get("/user/<id>")]
-pub fn get_user(id: u32) -> Json<User> {
+pub async fn get_user(id: u32, collections: &State<DBCollections>) -> Result<Json<User>, Status> {
+    let movie = match collections
+        .movies
+        .find_one(doc! { "title": "The Perils of Pauline" })
+        .await
+    {
+        Ok(movie) => movie,
+        Err(e) => {
+            eprintln!("Failed to find movie: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Found a movie:\n{:#?}", movie);
+
     let user = User {
         id,
         name: "John Doeeee".to_string(),
     };
-    Json(user)
+
+    Ok(Json(user))
 }
