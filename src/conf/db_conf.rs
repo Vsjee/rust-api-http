b8a3773be466f7733bdeb8debba49a8e24041a92
David Hernandez <davidfelipehernandez1@gmail.com>
2025-01-26 23:03:51 -0500

    feat: implement lazy initialization for MongoDB client and collections in db_conf.rs

--- src/conf/db_conf.rs
+++ src/conf/db_conf.rs
@@ -1,18 +1,49 @@
 use crate::constants::constants::MONGO_URI;
+use lazy_static::lazy_static;
+use mongodb::{bson::Document, Client, Collection, Database};
+use std::sync::Mutex;
 
-use mongodb::Client;
+lazy_static! {
+    static ref DB_CLIENT: Mutex<Option<Client>> = Mutex::new(None);
+    static ref DB: Mutex<Option<Database>> = Mutex::new(None);
+}
 
-#[allow(dead_code)]
-pub struct DBConf { }
+pub struct DBCollections {
+    // pub users: Collection<Document>,
+    pub movies: Collection<Document>,
+}
+
+impl DBCollections {
+    pub fn new(db: &Database) -> Self {
+        Self {
+            // users: db.collection("users"),
+            movies: db.collection("movies"),
+        }
+    }
+}
+
+pub struct DBConf {}
 
 impl DBConf {
-    pub async fn connect() -> Client {
-        return match Client::with_uri_str(MONGO_URI.as_str()).await {
+    pub async fn init() -> DBCollections {
+        let client = match Client::with_uri_str(MONGO_URI.as_str()).await {
             Ok(client) => client,
             Err(e) => {
                 eprintln!("Failed to connect to MongoDB: {}", e);
                 std::process::exit(1);
             }
         };
+
+        let db = client.database("sample_mflix");
+        let collections = DBCollections::new(&db);
+
+        *DB_CLIENT.lock().unwrap() = Some(client);
+        *DB.lock().unwrap() = Some(db);
+
+        collections
     }
+
+    // pub fn get_collections() -> Option<DBCollections> {
+    //     DB.lock().unwrap().as_ref().map(DBCollections::new)
+    // }
 }
