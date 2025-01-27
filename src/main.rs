f265b33bddf648876876773d76c8955d64bd6b9d
David Hernandez <davidfelipehernandez1@gmail.com>
2025-01-26 23:04:05 -0500

    feat: initialize MongoDB collections in main.rs and configure Rocket server

--- src/main.rs
+++ src/main.rs
@@ -35,28 +35,11 @@ async fn main() -> Result<(), rocket::Error> {
     dotenv().ok();
 
     // connect to MongoDB
-    let client = DBConf::connect().await;
-
-    let db: mongodb::Database = client.database("rust-api-http");
-    println!("Connected to MongoDB: {:?}", db);
-
-    println!("-----------");
-    let database = client.database("sample_mflix");
-    let my_coll: Collection<Document> = database.collection("movies");
-    let my_movie = match my_coll
-        .find_one(doc! { "title": "The Perils of Pauline" })
-        .await
-    {
-        Ok(movie) => movie,
-        Err(e) => {
-            eprintln!("Failed to find movie: {}", e);
-            std::process::exit(1);
-        }
-    };
-    println!("Found a movie:\n{:#?}", my_movie);
+    let collections = DBConf::init().await;
 
     // start rocket server and configure routes
     let _rocket = rocket::build()
+        .manage(collections)
         .mount("/", routes![index])
         .mount("/api/v1", routes![get_user])
         .launch()
